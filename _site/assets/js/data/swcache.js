11f4d2f1d0333b026c4cc906f7ff78daf6b89aac
maxawake <max.m.richter@protonmail.com>
2023-08-31 14:43:56 +0200

    hopefully not everything is fine

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -42,7 +42,7 @@ const resource = [
 const allowedDomains = [
     
 
-    'localhost:4000',
+    'maxawake.github.io',
 
     
         'chirpy-img.netlify.app',
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230831.144131';
+const cacheName = 'chirpy-20230831.144341';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
