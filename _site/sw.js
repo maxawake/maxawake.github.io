f16328877576b55e9b7869685bd62ca56ca198f5
maxawake <max.m.richter@protonmail.com>
2023-08-31 15:16:10 +0200

    lets see

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230831.210335';
+const cacheName = 'chirpy-20230831.211600';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
